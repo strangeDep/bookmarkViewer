6e1d41994f8115a03455708ae0cf77200830cb5a
yunju <dbswn731@doub.co.kr>
2023-11-14 17:06:14 +0900

    뒤로가기 업데이트

--- src/components/Contents.vue
+++ src/components/Contents.vue
@@ -13,6 +13,18 @@ const props = defineProps({
 const isShow = ref(false) // display 여부
 const childBookList = ref([]) // 하위 자식 북마크 리스트
 const prevBookList = ref([]) // 상위 북마크
+const parentId = ref('1')
+
+const insertBookmark = () => {
+
+  chrome.bookmarks.create({
+    'parentId': parentId.value,
+    'title': '가나다라',
+    'url': 'https://www.naver.com/',
+  });
+
+  childBookList.value = props.bookList
+}
 
 // ----------------------------------------------------------------------------------
 // Content Show
@@ -37,7 +49,7 @@ const hideContents = () => {
 // ----------------------------------------------------------------------------------
 const fnClick = (item) => {
   console.log(item)
-
+  parentId.value = item.id
   if(item.children != undefined){
     prevBookList.value = childBookList.value
     childBookList.value = item.children
@@ -51,8 +63,12 @@ const fnClick = (item) => {
 // ----------------------------------------------------------------------------------
 const fnBackClick = () => {
 
-  childBookList.value = prevBookList.value
-
+  if(childBookList.value == prevBookList.value) { // 폴더 안에 폴더
+    childBookList.value = props.bookList
+  } else {
+    childBookList.value = prevBookList.value
+  }
+  //childBookList.value = prevBookList.value
 }
 
 // ----------------------------------------------------------------------------------
@@ -75,13 +91,14 @@ defineExpose({
 
   <!--isShow 가 true 일때만 화면에 표시-->
   <section class="py-5" v-if="isShow">
-    <button @click="fnBackClick">뒤로가기</button>
+    <button @click="fnBackClick" v-show="childBookList != props.bookList">뒤로가기</button>
     <div class="container px-4 px-lg-5 mt-5">
       <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
         <div class="col mb-5" v-for="(item, index) in childBookList" :key="index" @click="fnClick(item)">
           <div class="card h-100">
             <div class="text-center">
               <p v-if="item.children != undefined">폴더</p>
+
               <p v-else>링크</p>
             </div>
             <!-- Product image-->
@@ -90,18 +107,42 @@ defineExpose({
             <div class="card-body p-4">
               <div class="text-center">
                 <!-- Product name-->
-                <h5 class="fw-bolder">{{item.title}}</h5>
+              <h5 class="fw-bolder"><a :href="item.url" target="_blank">{{item.title}}</a></h5><!--새 창 띄워서 이동-->
                 <!-- Product price-->
 
               </div>
             </div>
             <!-- Product actions-->
             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-              <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">{{item.link}}</a></div>
+<!--              <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">{{item.link}}</a></div>-->
+              <button>수정</button>
+              <button>삭제</button>
             </div>
           </div>
         </div>
+<!--        /////////////////-->
+        <div class="col mb-5">
+          <div class="card h-100">
+            <div class="text-center">
+              <p>추가</p>
+            </div>
+            <!-- Product image-->
+            <img class="card-img-top" src="../assets/plus.png" alt="..." @click="insertBookmark"/>
+            <!-- Product details-->
+            <div class="card-body p-4">
+              <div class="text-center">
+                <!-- Product name-->
+<!--                <h5 class="fw-bolder">추가</h5>-->
+                <!-- Product price-->
 
+              </div></div>
+            <!-- Product actions-->
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+              <button>등록</button>
+              <button>취소</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
